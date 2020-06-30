9f7514714e288b7cb3bc971415b9567fb01eb433
Ben Russell <benjaminrussell36@gmail.com>
2020-06-30 16:26:16 -0700

    Add function to countdown and to have a lose condition.
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- src/hungry-bear.js
+++ src/hungry-bear.js
@@ -17,10 +17,11 @@ export class HungryBear {
 
   didYouGetEaten() {
     if (this.foodLevel > 0 && this.moodLevel > 0 && this.hugLevel > 0) {
-      return false;
-    } else {
-      return true;
-    }
+      return $("#food").html(this.foodLevel);
+    } else if (this.foodLevel ===0 || this.moodLevel === 0 || this.hugLevel === 0) {
+      clearInterval(this.foodLevel, this.moodLevel, this.hugLevel);
+      $("#countdown").html("You got eaten!");
+    }  
   }
 
   feed() {
--- src/main.js
+++ src/main.js
@@ -6,22 +6,37 @@ import { HungryBear } from './../src/hungry-bear';
 
 $(document).ready(function() {
   let fuzzy = new HungryBear("Fuzzy");
-  fuzzy.setLevels();
-  $("#countdown").show();
+  $("#countdown").show(); // setting another interval that calls fuzzy.setLevels() every second
+  let counter = setInterval(timer, 1000);
+  fuzzy.setLevels(); //setting an interval here that decrements by 1 every second
+  function timer() {
+    console.log("running interval");
+    let food = fuzzy.foodLevel;
+    let scratch = fuzzy.moodLevel;
+    let hug = fuzzy.hugLevel;
+    
+    if (food <= 0 || scratch <= 0 || hug <= 0) {
+      clearInterval(counter);
+      return $("#countdown").text("You were eaten!");
+    }
+    $("#food").text(food);
+    $("#scratch").text(scratch);
+    $("#hug").text(hug);
+  }
+
   $("#primary").click(function() {
     $("#food").html(fuzzy.foodLevel);
     fuzzy.feed();
   });
+  
   $("#success").click(function() {
     $("#scratch").html(fuzzy.moodLevel);
     fuzzy.scratch();
   });
+  
   $("#warning").click(function() {
     $("#hug").html(fuzzy.hugLevel);
     fuzzy.hug();
   });
 });
 
-// setTimeout(function(){
-//   alert("Hello friend! Maybe you should sign up for our newsletter!");
-// }, 2000);
