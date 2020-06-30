9561c0185faf376333402d676a8cffe4a9c53e58
Ben Russell <benjaminrussell36@gmail.com>
2020-06-30 11:00:19 -0700

    Add ids for food, scratch, and hug to show their levels in HTML.
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
-import { HungryBear } from './hungry-bear';
+import { HungryBear } from './../src/hungry-bear';
 
 $(document).ready(function() {
   $("#buttons").click(function(event) {
     event.preventDefault();
-    let fuzzy = new HungryBear().getTime();
+    let fuzzy = new HungryBear("Fuzzy");
+    $("#countdown").show();
+    $("#food").text(fuzzy.foodLevel);
+    $("#scratch").text(fuzzy.moodLevel);
+    $("#hug").text(fuzzy.hugLevel);
     fuzzy.setLevels();
-    $(".countdown").text(fuzzy.foodlevel);
+    
+    $(".btn-primary").text();
   });
 });
